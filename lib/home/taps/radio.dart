7c4ca21a35ab4240f1ab9e97d90511d8c20c9b26
Reham Hatem <rha772201@gmail.com>
2024-02-26 15:33:38 +0200

    radio

---
+++ lib/home/taps/radio.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class redio extends StatelessWidget {
+  static const String routeName="rad";
+  const redio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 412, height: 222,
+            margin: EdgeInsets.only(top: 150,bottom: 40),
+            child: Image(image: AssetImage("assets/images/radio_image.png"))),
+        Text("إذاعة القرآن الكريم",style: GoogleFonts.elMessiri(
+          fontSize: 25,
+          fontWeight: FontWeight.w600,
+          color: Color(0xff242424)
+        ),),
+        SizedBox(height: 20,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.skip_previous,size: 50,color: Color(0xffB7935F)),
+            Icon(Icons.play_arrow,size: 70,color: Color(0xffB7935F)),
+            Icon(Icons.skip_next,size: 50,color: Color(0xffB7935F)),
+          ],
+        )
+      ],
+    );
+  }
+}
